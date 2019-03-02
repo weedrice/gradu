39b07bbe91d9bb5c1b99c84a9bfd73abc9385e7b
weedrice <supp0rtyoo@gmail.com>
2019-03-02 17:23:49 +0900

    #9 Add BaseActivity, BaseViewModel

--- app/src/main/java/a3pmplusalpha/gradu/ui/base/BaseActivity.java
+++ app/src/main/java/a3pmplusalpha/gradu/ui/base/BaseActivity.java
@@ -1,4 +1,51 @@
 package a3pmplusalpha.gradu.ui.base;
 
-public class BaseActivity {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+
+public abstract class BaseActivity<T extends ViewDataBinding, V extends BaseViewModel>
+        extends AppCompatActivity {
+
+    private T binding;
+    private V viewModel;
+
+    public abstract
+    @LayoutRes
+    int getLayoutId();
+
+    public abstract int getBindingVariable();
+
+    public abstract V getViewModel();
+
+    public void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if(view != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            if(inputMethodManager != null) {
+                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dataBinding();
+    }
+
+    private void dataBinding() {
+        binding = DataBindingUtil.setContentView(this, getLayoutId());
+        binding.setLifecycleOwner(this);
+        this.viewModel = viewModel == null ? getViewModel() : viewModel;
+        binding.setVariable(getBindingVariable(), viewModel);
+        binding.executePendingBindings();
+    }
 }
--- app/src/main/java/a3pmplusalpha/gradu/ui/base/BaseViewModel.java
+++ app/src/main/java/a3pmplusalpha/gradu/ui/base/BaseViewModel.java
@@ -1,4 +1,23 @@
 package a3pmplusalpha.gradu.ui.base;
 
-public class BaseViewModel {
+import androidx.lifecycle.ViewModel;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+public class BaseViewModel extends ViewModel {
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    public void addDisposable(Disposable d) {
+        compositeDisposable.add(d);
+    }
+
+    public CompositeDisposable getCompositeDisposable() {
+        return compositeDisposable;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.clear();
+    }
 }
