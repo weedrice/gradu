a4e5589e47d1b8006471848048b237d4de51a3bc
weedrice <supp0rtyoo@gmail.com>
2019-03-14 00:56:06 +0900

    #15 Composite MainActivity MVP

--- app/src/main/java/a3pmplusalpha/gradu/ui/main/MainActivity.java
+++ app/src/main/java/a3pmplusalpha/gradu/ui/main/MainActivity.java
@@ -1,23 +1,18 @@
-package a3pmplusalpha.gradu.ui;
+package a3pmplusalpha.gradu.ui.main;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import a3pmplusalpha.gradu.BR;
 import a3pmplusalpha.gradu.R;
 import a3pmplusalpha.gradu.databinding.ActivityHomeBinding;
 import a3pmplusalpha.gradu.ui.base.BaseActivity;
-import a3pmplusalpha.gradu.util.ViewModelFactory;
+import a3pmplusalpha.gradu.ui.main.home.HomeFragment;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
-public class HomeActivity extends BaseActivity<ActivityHomeBinding, HomeViewModel> {
+public class MainActivity extends BaseActivity<ActivityHomeBinding, MainContract.Presenter> {
     private static final int HOME_CONTAINER_ID = R.id.fl_home_container;
-
-    ViewModelFactory factory;
-    private HomeViewModel viewModel;
     private Fragment currentFragment;
-    private ActivityHomeBinding binding;
 
     @Override
     public int getLayoutId() {
@@ -25,28 +20,22 @@ public class HomeActivity extends BaseActivity<ActivityHomeBinding, HomeViewMode
     }
 
     @Override
-    public int getBindingVariable() {
-        return BR.homeViewModel;
-    }
-
-    @Override
-    public HomeViewModel getViewModel() {
-        viewModel = ViewModelProviders.of(this, factory).get(HomeViewModel.class);
-        return viewModel;
+    public MainContract.Presenter getPresenter() {
+        return null;
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = getDataBinding();
 
-        if(savedInstanceState == null) {
-            currentFragment = new HomeFragment();
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(HOME_CONTAINER_ID, currentFragment)
-                    .commit();
-            binding.llBottomNavigation.ivHome.setImageResource(R.drawable.img_home_clicked);
-        }
+        String html = getIntent().getExtras().getString("html");
+
+        currentFragment = new HomeFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(HOME_CONTAINER_ID, currentFragment)
+                .commit();
+
+        binding.llBottomNavigation.ivHome.setImageResource(R.drawable.img_home_clicked);
     }
 }
