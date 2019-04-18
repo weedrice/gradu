477cb3042dbdc433d61417a5109662a1871592a6
weedrice <supp0rtyoo@gmail.com>
2019-04-18 18:29:32 +0900

    #17 Add ClassInfo Html Parse

--- app/src/main/java/a3pmplusalpha/gradu/util/HtmlParse.java
+++ app/src/main/java/a3pmplusalpha/gradu/util/HtmlParse.java
@@ -4,14 +4,17 @@ import android.util.Log;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import a3pmplusalpha.gradu.model.repository.Local.entity.UserInfo;
 
 public class HtmlParse {
     public static UserInfo getAccountInformation(String html) {
         Document doc = Jsoup.parse(html);
-        Log.d("DOC CREATED", "GOOD");
         Elements elements = doc.select("tr[height$=28] td");
 
         UserInfo userInfo = new UserInfo();
@@ -43,9 +46,36 @@ public class HtmlParse {
             }
         }
 
-        Log.d("USERINFO", userInfo.toString());
-
-
         return userInfo;
+    }
+
+    public static HashMap<String, ArrayList<ArrayList<String>>> getClassInfomation(String html) {
+        Document doc = Jsoup.parse(html);
+        Elements elements = doc.select("div[id^=div_2]");
+
+        //연도별 과목을 담기 위한 map
+        HashMap<String, ArrayList<ArrayList<String>>> yearMap = new HashMap<>();
+        for(Element table : elements) {
+            Elements contents = table.select("td");
+
+            ArrayList<ArrayList<String>> classes = new ArrayList<>();
+            String year = contents.get(0).text().split(" ")[0];
+            for(int i=9;i<contents.size();i += 8) {
+                //한과목의 정보를 담는 arrayList
+                ArrayList<String> classList = new ArrayList<>();
+                classList.add(contents.get(i).text());
+                classList.add(contents.get(i+1).text());
+                classList.add(contents.get(i+2).text());
+                classList.add(contents.get(i+3).text());
+                classList.add(contents.get(i+4).text());
+                classList.add(contents.get(i+5).text());
+
+                classes.add(classList);
+            }
+
+            yearMap.put(year, classes);
+        }
+
+        return yearMap;
     }
 }
