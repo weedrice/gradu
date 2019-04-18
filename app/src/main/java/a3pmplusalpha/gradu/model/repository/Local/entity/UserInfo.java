32d0c6f413d4e095e6acb6674a401edb6cd695a4
weedrice <supp0rtyoo@gmail.com>
2019-04-19 02:42:18 +0900

    #17 Add Info Classes

--- app/src/main/java/a3pmplusalpha/gradu/model/repository/Local/entity/UserInfo.java
+++ app/src/main/java/a3pmplusalpha/gradu/model/repository/Local/entity/UserInfo.java
@@ -1,14 +1,42 @@
 package a3pmplusalpha.gradu.model.repository.Local.entity;
 
-public class UserInfo {
-    String name;
-    String scholarship;
-    String major1;
-    String major2;
-    boolean maxCredit;
-    String stunum;
-    String semester;
+import android.os.Parcel;
+import android.os.Parcelable;
 
+import java.util.HashMap;
+
+public class UserInfo implements Parcelable {
+    private String name;
+    private String scholarship;
+    private String major1;
+    private String major2;
+    private boolean maxCredit;
+    private String stunum;
+    private String semester;
+
+    public UserInfo() { }
+
+    public UserInfo(String name, String scholarship, String major1,
+                    String major2, boolean maxCredit, String stunum,
+                    String semester, HashMap<String, ClassInfo> classInfo) {
+        this.name = name;
+        this.scholarship = scholarship;
+        this.major1 = major1;
+        this.major2 = major2;
+        this.maxCredit = maxCredit;
+        this.stunum = stunum;
+        this.semester = semester;
+    }
+
+    private UserInfo(Parcel in) {
+        this.name = in.readString();
+        this.scholarship = in.readString();
+        this.major1 = in.readString();
+        this.major2 = in.readString();
+        this.maxCredit = Boolean.valueOf(in.readString());
+        this.stunum = in.readString();
+        this.semester = in.readString();
+    }
 
     public String getName() {
         return name;
@@ -30,14 +58,6 @@ public class UserInfo {
         return major1;
     }
 
-    public boolean isMaxCredit() {
-        return maxCredit;
-    }
-
-    public void setMaxCredit(boolean maxCredit) {
-        this.maxCredit = maxCredit;
-    }
-
     public void setMajor1(String major1) {
         this.major1 = major1;
     }
@@ -50,17 +70,12 @@ public class UserInfo {
         this.major2 = major2;
     }
 
-    @Override
-    public String toString() {
-        return "UserInfo{" +
-                "name='" + name + '\'' +
-                ", scholarship='" + scholarship + '\'' +
-                ", major1='" + major1 + '\'' +
-                ", major2='" + major2 + '\'' +
-                ", maxCredit=" + maxCredit +
-                ", stunum='" + stunum + '\'' +
-                ", semester='" + semester + '\'' +
-                '}';
+    public boolean isMaxCredit() {
+        return maxCredit;
+    }
+
+    public void setMaxCredit(boolean maxCredit) {
+        this.maxCredit = maxCredit;
     }
 
     public String getStunum() {
@@ -77,5 +92,34 @@ public class UserInfo {
 
     public void setSemester(String semester) {
         this.semester = semester;
+    }
+
+    public static final Creator<UserInfo> CREATOR = new Creator<UserInfo>() {
+        @Override
+        public UserInfo createFromParcel(Parcel parcel) {
+            return new UserInfo(parcel);
+        }
+
+        @Override
+        public UserInfo[] newArray(int i) {
+            return new UserInfo[i];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(scholarship);
+        parcel.writeString(major1);
+        parcel.writeString(major2);
+        parcel.writeString(String.valueOf(maxCredit));
+        parcel.writeString(stunum);
+        parcel.writeString(semester);
     }
 }
